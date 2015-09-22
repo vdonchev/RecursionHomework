b8e30bde36fe28f7cb1e7dad081f133e762c4cba
xclusivePc <xmt@abv.bg>
2015-09-22 15:20:51 +0300

    update

--- 01.TowerOfHanoi/TowerOfHanoi.cs
+++ 01.TowerOfHanoi/TowerOfHanoi.cs
@@ -1,30 +1,38 @@
 ﻿namespace _01.TowerOfHanoi
 {
+    using System;
     using System.Collections.Generic;
     using System.Linq;
 
     class TowerOfHanoi
     {
-        static Stack<int> source = new Stack<int>(Enumerable.Range(1,3).Reverse());
-        static Stack<int> spare = new Stack<int>(); 
-        static Stack<int> destination = new Stack<int>();
+        private static int steps = 0;
 
         static void Main()
         {
+            Console.Write("Insert n = ");
+            int numOfDisks = int.Parse(Console.ReadLine());
+            Stack<int> source = new Stack<int>(Enumerable.Range(1, numOfDisks).Reverse());
+            Stack<int> spare = new Stack<int>();
+            Stack<int> destination = new Stack<int>();
 
+            MoveDisks(numOfDisks, source, destination, spare);
+
+            Console.WriteLine("Finished in {0} steps", steps);
         }
 
         static void MoveDisks(int bottomDisk, Stack<int> source, Stack<int> destination, Stack<int> spare)
         {
             if (bottomDisk == 1)
             {
+                steps++;
                 destination.Push(source.Pop());
             }
             else
             {
-                // TODO
+                MoveDisks(bottomDisk - 1, source, spare, destination);
                 destination.Push(source.Pop());
-                // TODO
+                MoveDisks(bottomDisk - 1, spare, destination, source);
             }
         }
     }
